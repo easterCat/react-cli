d2e47c8037f3aa742f881735bce02f7faea79518
easterCat <fuhuodemao@163.com>
2018-06-28 16:37:56 +0800

    adasd

---
+++ index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const inquirer = require('inquirer')
+
+const basename = path.basename(process.cwd())
+
+const questions = [
+    {
+        type: 'input',
+        name: 'appName',
+        message: 'App Name: ',
+        default: basename,
+    },
+    {
+        type: 'input',
+        name: 'version',
+        message: 'Version: ',
+        default: '1.0.0',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Description: '
+    },
+    {
+        type: 'checkbox',
+        name: 'loaders',
+        message: 'Loaders: ',
+        choices: ['sass', 'less'],
+        default: ['sass']
+    },
+    {
+        type: 'input',
+        name: 'extractPath',
+        message: 'ExtractTextPlugin path: '
+    },
+    {
+        type: 'checkbox',
+        name: 'options',
+        message: 'Options: ',
+        choices: [
+            {name: 'Use rctui', value: 'rctui'},
+            {name: 'Use css module', value: 'cssModule'},
+            {name: 'Use eslint', value: 'eslint'},
+            {name: 'Use taobao registry', value: 'taobao'},
+        ],
+        default: ['eslint', 'taobao']
+    }
+]
+
+inquirer.prompt(questions).then(async (config) => {
+    console.log('✨ Success.')
+})
